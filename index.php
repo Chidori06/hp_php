538d89b92f7fd7f9186c01623190e57bf0187b9a
Chidori06 <li.chidori.kinomoto@gmail.com>
2026-09-10 08:34:18 +0200

    mise en place des derniers points du brief

--- index.php
+++ index.php
@@ -10,27 +10,72 @@ include_once "logic.php";
     <title>Harry Potter : les persos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+        .death {
+            width: 50px;
+            height: 50px;
+        }
+
+        .house {
+            width: 50px;
+            height: 50px;
+        }
+
+        .card-img-top {
+            height: 350px;
+            object-fit: cover;
+        }
+    </style>
+
+
 </head>
 
 <body>
+    <h1 class="text-center my-4">
+        Personnages Harry Potter
+    </h1>
+    <p class="text-center mb-4">
+        Nombre de personnages affichés :
+        <?= $nbPersonnages ?>
+    </p>
     <div class="container">
         <div class="row text-center">
             <?php foreach ($dataDecode as $chara): ?>
-                <div class="col-md-3 mb-4">
-                    <?php if (!empty($chara['image'])): ?>
-                        <div class="card h-100">
-                            <img src="<?= $chara['image'] ?>" class="card-img-top" alt="...">
+                <?php if (!empty($chara['image'])): ?>
+                    <div class="col-md-3 mb-4">
+                        <div class="card h-100 <?= getBorder($chara['gender']) ?> ">
+                            <div class="position-relative">
+                                <img src="<?= $chara['image'] ?>" class="card-img-top" alt="<?= $chara['name'] ?>">
+                                <div class="position-absolute top-0 start-0">
+                                    <?php if (!empty($chara['house'])): ?>
+                                        <img src="<?= getHouse($chara['house']) ?>" class="house rounded-circle"
+                                            alt="Image des maisons">
+                                    <?php endif; ?>
+                                </div>
+                                <?php if (isset($chara['alive']) && $chara['alive'] === false): ?>
+                                    <div class="position-absolute top-0 end-0 death">
+                                        💀
+                                    </div>
+                                <?php endif; ?>
+                            </div>
                             <div class="card-body">
                                 <h5 class="card-title"><?= $chara['name'] ?></h5>
                                 <h6 class="card-text"><?= $chara['house'] ?></h6>
                                 <p class="card-text"><?= $chara['dateOfBirth'] ?></p>
+                                <?php if (!empty($chara['yearOfBirth'])): ?>
+                                    <p class="card-text">
+                                        Âge :
+                                        <?= getAge($chara['yearOfBirth']) ?> ans
+                                    </p>
+                                <?php endif; ?>
                             </div>
                         </div>
-                    <?php endif; ?>
-                </div>
+                    </div>
+                <?php endif; ?>
             <?php endforeach; ?>
         </div>
     </div>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
--- logic.php
+++ logic.php
@@ -2,5 +2,56 @@
 
 $data = file_get_contents("https://hp-api.onrender.com/api/characters");
 $dataDecode = json_decode($data, true);
-// var_dump($dataDecode);
+
+$nbPersonnages = 0;
+
+foreach ($dataDecode as $chara) {
+    if (!empty($chara['image'])) {
+        $nbPersonnages++;
+    }
+}
+
+
+function getBorder($gender)
+{
+    if ($gender === 'male') {
+        return 'border-primary';
+    } elseif ($gender === 'female') {
+        return 'border-danger';
+    }
+
+    return 'border-dark';
+}
+
+function getHouse($house)
+{
+    switch ($house) {
+        case "Gryffindor":
+            return 'https://cdn.discordapp.com/attachments/1546429605630709780/1547251492883406958/Gryffindor.jpeg?ex=6aa2bdbe&is=6aa16c3e&hm=52e406254586702d1fd987b814ea3e4ab38bf1e6a3c63865ad17385147e1bf52&';
+
+        case "Slytherin":
+            return 'https://cdn.discordapp.com/attachments/1546429605630709780/1547251492417708123/Slytherin.jpeg?ex=6aa2bdbe&is=6aa16c3e&hm=4c562c942da806934557c00cc4323dfadedfc4579a2a4d56ce7d4db5e9675471&';
+
+        case "Hufflepuff":
+            return 'https://cdn.discordapp.com/attachments/1546429605630709780/1547251493558812762/Hufflepuff.jpeg?ex=6aa2bdbe&is=6aa16c3e&hm=c3a3a76137fc193e99cc2284ff312c40be30698f79eff1143f1fc0d6b3e454d2&';
+
+        case "Ravenclaw":
+            return 'https://cdn.discordapp.com/attachments/1546429605630709780/1547251494011666542/Ravenclaw.jpeg?ex=6aa2bdbe&is=6aa16c3e&hm=5fc8425ba578fa0044dc3e6b27c8952e1a8b78f751a63fa7c1ab7cd32552fafb&';
+
+        default:
+            return '';
+    }
+}
+
+function getAge($yearOfBirth)
+{
+    if (empty($yearOfBirth)) {
+        return null;
+    }
+
+    $currentYear = date('Y');
+
+    return $currentYear - $yearOfBirth;
+}
+
 ?>
